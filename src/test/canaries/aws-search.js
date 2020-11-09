aab941b183f04c072f7a71f2a5e758bac6b7cc98
Chris McGowan <chris@anthum.com>
2020-11-09 12:02:14 -0500

    add enhanced Params module with examples

--- src/test/canaries/aws-search.js
+++ src/test/canaries/aws-search.js
@@ -8,28 +8,19 @@ async function runTests() {
 }
 
 async function testAwsSearch(page) {
-  /* Example using SSM Parameter Store:
-   *
-   *   const params = {
-   *     searchHome:  await Params.get('searchHome'),
-   *     searchQuery: await Params.get('searchQuery'),
-   *   }
-   */
-
-  // For demo only. These would be obtained dynamically (env vars, Paramater Store, etc)
-  const params = {
-    searchHome:  'https://aws.amazon.com/search/',
-    searchQuery: 'cloudwatch synthetics',
-  }
+  const [searchHome, searchQuery] = await Promise.all([
+    Params.get('searchHome'),
+    Params.get('searchQuery'),
+  ])
 
   info('opening AWS search home...')
-  await page.goto(params.searchHome)
+  await page.goto(searchHome)
   const searchBox = await page.waitForSelector('#awsm-search-form input[type=text]', { visible: true })
   info('at search home', page.url())
   await screenshot(page, 'search', 'home')
 
   info('running search...')
-  await searchBox.type(params.searchQuery)
+  await searchBox.type(searchQuery)
   await page.keyboard.press('Enter')
   await page.waitForSelector('#aws-search-result .lb-search-items', { visible: true })
   info('at search results')
--- src/test/canaries/util/params.js
+++ src/test/canaries/util/params.js
@@ -1,86 +1,108 @@
-/*  AWS Systems Manager (SSM) Parameter Store Utilities
+/*  Dynamic Parameter Value Utilities
  *
- *  This module provides utilities for fetching values from an SSM Parameter
- *  Store. To ensure runtime reliability, all parameters defined here must also
- *  be defined in the Parameter Store, otherwise an Error is thrown.
+ *  This module provides all dynamic runtime parameter values obtained from
+ *  static values, environment variables, or AWS Systems Manager (SSM)
+ *  Parameter Store. To ensure runtime reliability, all parameters defined here
+ *  must also be defined an available in their respective locations, otherwise
+ *  an Error is thrown.
  *
  *  To configure credentials for the aws-sdk, please see https://amzn.to/35RxhVU
  */
 
 
-const { info, error } = require('./runtime')
+const { debug, info, error } = require('./runtime')
 const AWS = require('aws-sdk')
-const ssm = new AWS.SSM()
+
+// these are async populated and returned
+let initilizedValues = null
+
+// Start defining parameters here. Use values or functions
+const params = {
+  searchHome:  async () => {
+    // this example demonstrates dynamic params that resolved during runtime
+    info('getting searchHome value...')
+    await new Promise(resolve => setTimeout(resolve, 2000))
+    return 'https://aws.amazon.com/search/'
+  },
+  searchQuery: 'cloudwatch synthetics',
+}
 
 
 /*  Define SSM Parameter Store names and the local values they'll be mapped to.
  *  Use format: <REMOTE_NAME>: <LOCAL_EXPORTED_NAME>
  *
- *     const paramNameMap = {
+ *     const ssmParamNameMap = {
  *       '/my-webapp-name/username': 'user',
  *       '/my-webapp-name/password': 'pass',
  *     }
  */
-const paramNameMap = {}
-
-
-// Waits until first param is requested to fetch from SSM Parameter Store
-const fetchParams = (() => {
-  let fetchPromise = null
-
-  return () => {
-    if (!fetchPromise) {
-      info('fetching values from Parameter Store...')
-
-      const remoteNames = Object.keys(paramNameMap)
-      if (!remoteNames.length) {
-        return Promise.resolve([])
-      }
-
-      // API limits 10 per request. Refactor with multiple API requests if needed
-      if (remoteNames.length > 10)
-        throw Error('Parameters exceed maximum allowed (code refactor needed.')
-      
-      fetchPromise = new Promise(async (resolve, reject) => {
-        const reqParams = {
-          Names: remoteNames,
-          WithDecryption: true,
-        }
-        const response = await ssm.getParameters(reqParams).promise()
-        const invalidParams = response.InvalidParameters
-        if (invalidParams.length) {
-          const msg = 'required params do not exist in SSM Parameter Store'
-          error(msg, invalidParams)
-          throw Error(msg)
-        }
-
-        const params = {}
-        response.Parameters.forEach(({ Name, Value }) => {
-          params[ paramNameMap[Name] ] = Value
-        })
-
-        // If needed, convert multi-line values to arrays here
-        // params.exampleValue = arrayFromLines(params.exampleValue)
-
-        resolve(params)
-      })
-    }
-
-    return fetchPromise
-  }
-})()
+const ssmParamNameMap = {}
 
 
 const Params = {
   get: async (name) => {
-    const params = await fetchParams()
+    if (!initilizedValues) {
+      initilizedValues = initValues()
+    }
+    const values = await initilizedValues
 
     // this requirement can be optional, depending on your app
-    if (!params.hasOwnProperty(name))
+    if (!values.hasOwnProperty(name))
       throw Error('Invalid parameter name: '+name)
 
-    return params[name]
+    let value = values[name]
+
+    // if value is a function, swap it with the return value
+    if ('function' === typeof value) {
+      values[name] = value.call(values)
+      value = await values[name]
+    }
+
+    return value
   }
+}
+
+
+function initValues() {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const ssmValues = await fetchSsmParams()
+      resolve(Object.assign({}, ssmValues, params))
+    } catch (e) {
+      reject(e)
+    }
+  })
+}
+
+
+// Fetch defined param names from SSM Parameter Store.  Throw error if any do not exist
+async function fetchSsmParams() {
+  const remoteNames = Object.keys(ssmParamNameMap)
+  if (!remoteNames.length) {
+    return {}
+  }
+
+  // API limits 10 per request. Refactor with multiple API paging requests if needed
+  if (remoteNames.length > 10)
+    throw Error('SSM Parameters exceed maximum allowed (code refactor needed).')
+  
+  info('fetching values from Parameter Store...')
+  const ssm = new AWS.SSM()
+  const reqParams = {
+    Names: remoteNames,
+    WithDecryption: true,
+  }
+  const response = await ssm.getParameters(reqParams).promise()
+  const invalidParams = response.InvalidParameters
+  if (invalidParams.length) {
+    const msg = 'required params do not exist in SSM Parameter Store'
+    throw Error(`${msg}: [\n  ${invalidParams.join('\n  ')}\n]`)
+  }
+
+  response.Parameters.forEach(({ Name, Value }) => {
+    // If needed, convert multi-line values to arrays here
+    params[ ssmParamNameMap[Name] ] = arrayFromLines(Value)
+  })
 }
 
 
